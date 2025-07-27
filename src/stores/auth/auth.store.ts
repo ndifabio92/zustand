e5b5240f72d567e9d3670bc6300174e2c896371c
Nicolas <nicolasdifabio92@gmail.com>
2025-07-27 00:26:12 -0300

    .

--- src/stores/auth/auth.store.ts
+++ src/stores/auth/auth.store.ts
@@ -1,65 +1,55 @@
-import { StateCreator, create } from 'zustand';
-import type { AuthStatus, User } from '../../interfaces';
-import { AuthService } from '../../services/auth.service';
-import { devtools, persist } from 'zustand/middleware';
-
-
+import { StateCreator, create } from "zustand";
+import type { AuthStatus, User } from "../../interfaces";
+import { AuthService } from "../../services/auth.service";
+import { devtools, persist } from "zustand/middleware";
 
 export interface AuthState {
-
   status: AuthStatus;
   token?: string;
   user?: User;
 
-  loginUser: ( email: string, password: string ) => Promise<void>;
+  loginUser: (email: string, password: string) => Promise<void>;
   checkAuthStatus: () => Promise<void>;
   logoutUser: () => void;
 }
 
-
-const storeApi: StateCreator<AuthState> = ( set ) => ( {
-
-  status: 'pending',
+const storeApi: StateCreator<AuthState> = (set) => ({
+  status: "pending",
   token: undefined,
   user: undefined,
 
-
-  loginUser: async ( email: string, password: string ) => {
-
+  loginUser: async (email: string, password: string) => {
     try {
-      const { token, ...user } = await AuthService.login( email, password );
-      set( { status: 'authorized', token, user } );
-
-    } catch ( error ) {
-      set( { status: 'unauthorized', token: undefined, user: undefined } );
-      throw 'Unauthorized';
+      // const { token, ...user } = await AuthService.login(email, password);
+      const token = "asdfasdf";
+      const user: User = {
+        id: "123",
+        email,
+        fullName: "John Doe",
+        isActive: true,
+        roles: ["user"],
+      };
+      set({ status: "authorized", token, user });
+    } catch (error) {
+      set({ status: "unauthorized", token: undefined, user: undefined });
+      throw "Unauthorized";
     }
-
   },
 
   checkAuthStatus: async () => {
-
     try {
       const { token, ...user } = await AuthService.checkStatus();
-      set( { status: 'authorized', token, user } );
-
-    } catch ( error ) {
-      set( { status: 'unauthorized', token: undefined, user: undefined } );
+      set({ status: "authorized", token, user });
+    } catch (error) {
+      set({ status: "unauthorized", token: undefined, user: undefined });
     }
   },
 
   logoutUser: () => {
-    set( { status: 'unauthorized', token: undefined, user: undefined } );
-  }
-
-} );
-
+    set({ status: "unauthorized", token: undefined, user: undefined });
+  },
+});
 
 export const useAuthStore = create<AuthState>()(
-  devtools(
-    persist(
-      storeApi,
-      { name: 'auth-storage' }
-    )
-  )
+  devtools(persist(storeApi, { name: "auth-storage" }))
 );
